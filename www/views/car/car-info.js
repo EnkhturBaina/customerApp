be89c7b9ab07392555fbaba01000b65eb06e0ff6
Ganbayaraas <ganbayaraas@yahoo.com>
2021-02-16 20:59:38 +0800

    bankList

--- www/views/car/car-info.js
+++ www/views/car/car-info.js
@@ -1,104 +1,106 @@
-angular.module("carinfo.Ctrl", []).controller("carinfoCtrl", function ($rootScope, $state, serverDeferred, $window, $scope, $ionicSlideBoxDelegate) {
-  $scope.Math = $window.Math;
-  $rootScope.newReqiust = {};
+angular.module("carinfo.Ctrl", []).controller("carinfoCtrl", function($rootScope, $state, serverDeferred, $window, $scope, $ionicSlideBoxDelegate) {
+    $scope.Math = $window.Math;
+    $rootScope.newReqiust = {};
 
-  // auto Leasing
-  $scope.goAutoleasing = function () {
-    var next = $rootScope.checkLoginUserDatas("car-info", "autoleasing-2");
-    $state.go(next.now, { path: next.nextpath });
-  };
-  // get car Data
-  $scope.getCarinfo = function () {
-    $scope.isCarDetail = true;
-    serverDeferred.request("PL_MDVIEW_004", { systemmetagroupid: "1597654926672135", id: $rootScope.selectedCarData.id }).then(function (response) {
-      if (!isEmpty(response[0]) && isObject(response[0])) {
-        if (response[0].text10 == "101") {
-          $scope.isCarDetail = false;
-        } else if (response[0].text10 == "102") {
-          $scope.isCarDetail = true;
+    // auto Leasing
+    $scope.goAutoleasing = function() {
+        var next = $rootScope.checkLoginUserDatas("car-info", "autoleasing-2");
+        $state.go(next.now, { path: next.nextpath });
+    };
+    // get car Data
+    $scope.getCarinfo = function() {
+        $scope.isCarDetail = true;
+        serverDeferred.request("PL_MDVIEW_004", { systemmetagroupid: "1597654926672135", id: $rootScope.selectedCarData.id }).then(function(response) {
+            if (!isEmpty(response[0]) && isObject(response[0])) {
+                if (response[0].text10 == "101") {
+                    $scope.isCarDetail = false;
+                } else if (response[0].text10 == "102") {
+                    $scope.isCarDetail = true;
+                }
+                var images = [];
+                if (response[0].itempic) images.push(response[0].itempic);
+                if (response[0].itempic2) images.push(response[0].itempic2);
+                if (response[0].itempic3) images.push(response[0].itempic3);
+                if (response[0].itempic4) images.push(response[0].itempic4);
+                if (response[0].itempic5) images.push(response[0].itempic5);
+                if (response[0].itempic6) images.push(response[0].itempic6);
+                if (response[0].itempic7) images.push(response[0].itempic7);
+                if (response[0].itempic8) images.push(response[0].itempic8);
+                response[0].images = images;
+                $scope.carData = response[0];
+                $rootScope.selectedCarData = response[0];
+                $scope.getbankData();
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+                $ionicSlideBoxDelegate.update();
+            }
+        });
+    };
+    $scope.getbankData = function() {
+        $rootScope.bankList = [];
+        if (!isEmpty($rootScope.selectedCarData) && !isEmpty($rootScope.selectedCarData.itemcode)) {
+            // { type: "car", operation: "calculation", productCode: $rootScope.selectedCarData.itemcode }
+            serverDeferred.carCalculation({ "type": "allBanks" }).then(function(response) {
+                $rootScope.bankList = response.result.data;
+                console.log(response.result.data);
+                // console.log(response);
+            });
         }
-        var images = [];
-        if (response[0].itempic) images.push(response[0].itempic);
-        if (response[0].itempic2) images.push(response[0].itempic2);
-        if (response[0].itempic3) images.push(response[0].itempic3);
-        if (response[0].itempic4) images.push(response[0].itempic4);
-        if (response[0].itempic5) images.push(response[0].itempic5);
-        if (response[0].itempic6) images.push(response[0].itempic6);
-        if (response[0].itempic7) images.push(response[0].itempic7);
-        if (response[0].itempic8) images.push(response[0].itempic8);
-        response[0].images = images;
-        $scope.carData = response[0];
-        $rootScope.selectedCarData = response[0];
-        $scope.getbankData();
-        if (!$scope.$$phase) {
-          $scope.$apply();
+    };
+    $scope.getCarinfo();
+
+    // basket
+    $scope.addtoBasket = function() {
+        var value = searchJsonValue($rootScope.basketData, "id", $rootScope.selectedCarData.id);
+        $rootScope.selectedCarData.itemquantity = "1";
+        if (isEmpty(value)) {
+            $rootScope.basketData.push($rootScope.selectedCarData);
+            localStorage.setItem("basketData", JSON.stringify($rootScope.basketData));
+            $rootScope.alert("Сагсанд нэмэгдлээ");
+        } else {
+            $rootScope.alert("Сагслагдсан байна");
         }
-        $ionicSlideBoxDelegate.update();
-      }
-    });
-  };
-  $scope.getbankData = function () {
-    $rootScope.bankList = [];
-    if (!isEmpty($rootScope.selectedCarData) && !isEmpty($rootScope.selectedCarData.itemcode)) {
-      serverDeferred.carCalculation({ type: "car", operation: "calculation", productCode: $rootScope.selectedCarData.itemcode }).then(function (response) {
-        $rootScope.bankList = response.result;
-        // console.log(response);
-      });
-    }
-  };
-  $scope.getCarinfo();
+    };
 
-  // basket
-  $scope.addtoBasket = function () {
-    var value = searchJsonValue($rootScope.basketData, "id", $rootScope.selectedCarData.id);
-    $rootScope.selectedCarData.itemquantity = "1";
-    if (isEmpty(value)) {
-      $rootScope.basketData.push($rootScope.selectedCarData);
-      localStorage.setItem("basketData", JSON.stringify($rootScope.basketData));
-      $rootScope.alert("Сагсанд нэмэгдлээ");
-    } else {
-      $rootScope.alert("Сагслагдсан байна");
-    }
-  };
+    $scope.next = function() {
+        $ionicSlideBoxDelegate.next();
+    };
+    $scope.previous = function() {
+        $ionicSlideBoxDelegate.previous();
+    };
+    $scope.slideChanged = function(index) {
+        $scope.slideIndex = index;
+    };
+    $rootScope.newReqiust = {};
 
-  $scope.next = function () {
-    $ionicSlideBoxDelegate.next();
-  };
-  $scope.previous = function () {
-    $ionicSlideBoxDelegate.previous();
-  };
-  $scope.slideChanged = function (index) {
-    $scope.slideIndex = index;
-  };
-  $rootScope.newReqiust = {};
-
-  $scope.payOn = function () {
-    document.getElementById("paypopup").style.display = "block";
-  };
-  $scope.payOff = function () {
-    document.getElementById("paypopup").style.display = "none";
-  };
-  $scope.tabhide = function () {
-    document.getElementById("home-tab").style.display = "none";
-  };
-  $scope.collapse = function () {
-    document.getElementById("addition-info").style.display = "block";
-    document.getElementById("collapse-btn").style.display = "none";
-    document.getElementById("uncollapse-btn").style.display = "block";
-  };
-  $scope.uncollapse = function () {
-    document.getElementById("addition-info").style.display = "none";
-    document.getElementById("collapse-btn").style.display = "block";
-    document.getElementById("uncollapse-btn").style.display = "none";
-  };
-  $scope.shouldHide = function () {
-    switch ($state.current.name) {
-      case "statename1":
-        return true;
-      case "statename2":
-        return true;
-      default:
-        return false;
-    }
-  };
+    $scope.payOn = function() {
+        document.getElementById("paypopup").style.display = "block";
+    };
+    $scope.payOff = function() {
+        document.getElementById("paypopup").style.display = "none";
+    };
+    $scope.tabhide = function() {
+        document.getElementById("home-tab").style.display = "none";
+    };
+    $scope.collapse = function() {
+        document.getElementById("addition-info").style.display = "block";
+        document.getElementById("collapse-btn").style.display = "none";
+        document.getElementById("uncollapse-btn").style.display = "block";
+    };
+    $scope.uncollapse = function() {
+        document.getElementById("addition-info").style.display = "none";
+        document.getElementById("collapse-btn").style.display = "block";
+        document.getElementById("uncollapse-btn").style.display = "none";
+    };
+    $scope.shouldHide = function() {
+        switch ($state.current.name) {
+            case "statename1":
+                return true;
+            case "statename2":
+                return true;
+            default:
+                return false;
+        }
+    };
 });
--- www/views/home/home.js
+++ www/views/home/home.js
@@ -1,133 +1,131 @@
-﻿angular.module("home.Ctrl", []).controller("homeCtrl", function ($scope, $ionicPopup, $ionicLoading, serverDeferred, $ionicSlideBoxDelegate, $cordovaNetwork, $rootScope, $ionicTabsDelegate, $timeout) {
-  $rootScope.serverUrl = "http://dev.veritech.mn:8082/erp-services/RestWS/runJson";
-  $rootScope.imagePath = "https://dev.veritech.mn/";
-  // $rootScope.serverUrl = "http://leasing.digitalcredit.mn:8080/erp-services/RestWS/runJsonz";
-  // $rootScope.imagePath = "http://leasing.digitalcredit.mn/";
-  $rootScope.serverHeader = { "content-type": "application/json;charset=UTF-8" };
-  $rootScope.sessionid = "65178215-7896-4513-8e26-896df9cb36ad";
-  $cordovaNetwork.isOnline = function () {
-    return true;
-  };
-  $rootScope.newCarReq = {};
-  $rootScope.hideFooter = true;
-  // ========= Slide =============
-  $scope.activeSlideIndex = 0;
-  $rootScope.showBanner = true;
-  $scope.getBanner = function () {
-    document.getElementsByTagName("ion-nav-bar")[0].style.visibility = "hidden";
-    $scope.bannerData = JSON.parse(localStorage.getItem("banner"));
-    serverDeferred.requestFull("PL_MDVIEW_004", { systemmetagroupid: "1597631698242718" }).then(function (data) {
-      // console.log(data);
-      delete data[1].aggregatecolumns;
-      delete data[1].paging;
-      if (!isEmpty(data[1])) {
-        $scope.bannerData = data[1];
-        localStorage.setItem("banner", JSON.stringify(data[1]));
-        $ionicSlideBoxDelegate.update();
-      }
-    });
-  };
-  $scope.slideChanged = function (index) {
-    $scope.activeSlideIndex = index;
-  };
-  $scope.nextSlideChanged = function () {
-    $ionicSlideBoxDelegate.next();
-    // $rootScope.showBanner = false;
-  };
-  $scope.endSlideChanged = function (index) {
-    $ionicSlideBoxDelegate.slide(Object.keys($scope.bannerData).length - 1, 0);
-    //$scope.activeSlideIndex = $scope.bannerData.length - 1;
-  };
-  $scope.showlogin = function () {
-    document.getElementsByTagName("ion-nav-bar")[0].style.visibility = "visible";
-    $rootScope.showBanner = false;
-    $rootScope.hideFooter = false;
-  };
+﻿angular.module("home.Ctrl", []).controller("homeCtrl", function($scope, $ionicPopup, $ionicLoading, serverDeferred, $ionicSlideBoxDelegate, $cordovaNetwork, $rootScope, $ionicTabsDelegate, $timeout) {
+    // $rootScope.serverUrl = "http://dev.veritech.mn:8082/erp-services/RestWS/runJson";
+    // $rootScope.imagePath = "https://dev.veritech.mn/";
+    $rootScope.serverUrl = "http://leasing.digitalcredit.mn:8080/erp-services/RestWS/runJsonz";
+    $rootScope.imagePath = "http://leasing.digitalcredit.mn/";
+    $rootScope.serverHeader = { "content-type": "application/json;charset=UTF-8" };
+    $rootScope.sessionid = "65178215-7896-4513-8e26-896df9cb36ad";
+    $cordovaNetwork.isOnline = function() {
+        return true;
+    };
+    $rootScope.newCarReq = {};
+    $rootScope.hideFooter = true;
+    // ========= Slide =============
+    $scope.activeSlideIndex = 0;
+    $rootScope.showBanner = true;
+    $scope.getBanner = function() {
+        document.getElementsByTagName("ion-nav-bar")[0].style.visibility = "hidden";
+        $scope.bannerData = JSON.parse(localStorage.getItem("banner"));
+        serverDeferred.requestFull("PL_MDVIEW_004", { systemmetagroupid: "1597631698242718" }).then(function(data) {
+            // console.log(data);
+            delete data[1].aggregatecolumns;
+            delete data[1].paging;
+            if (!isEmpty(data[1])) {
+                $scope.bannerData = data[1];
+                localStorage.setItem("banner", JSON.stringify(data[1]));
+                $ionicSlideBoxDelegate.update();
+            }
+        });
+    };
+    $scope.slideChanged = function(index) {
+        $scope.activeSlideIndex = index;
+    };
+    $scope.nextSlideChanged = function() {
+        $ionicSlideBoxDelegate.next();
+        // $rootScope.showBanner = false;
+    };
+    $scope.endSlideChanged = function(index) {
+        $ionicSlideBoxDelegate.slide(Object.keys($scope.bannerData).length - 1, 0);
+        //$scope.activeSlideIndex = $scope.bannerData.length - 1;
+    };
+    $scope.showlogin = function() {
+        document.getElementsByTagName("ion-nav-bar")[0].style.visibility = "visible";
+        $rootScope.showBanner = false;
+        $rootScope.hideFooter = false;
+    };
 
-  $scope.isBanneNotShowChecked = { checked: false };
-  $scope.isNotShow = function () {
-    if ($scope.isBanneNotShowChecked.checked == true) {
-      localStorage.setItem("bannerNotShow", JSON.stringify($scope.isBanneNotShowChecked.checked));
-    } else {
-      localStorage.removeItem("bannerNotShow");
-    }
-  };
-  $scope.getBanner();
+    $scope.isBanneNotShowChecked = { checked: false };
+    $scope.isNotShow = function() {
+        if ($scope.isBanneNotShowChecked.checked == true) {
+            localStorage.setItem("bannerNotShow", JSON.stringify($scope.isBanneNotShowChecked.checked));
+        } else {
+            localStorage.removeItem("bannerNotShow");
+        }
+    };
+    $scope.getBanner();
 
-  // ============= glob ========================
-  $rootScope.alert = function (messege) {
-    if (!isEmpty($scope.alertPopup)) {
-      $scope.alertPopup.close();
-    }
-    $scope.alertPopup = $ionicPopup.alert({
-      title: "",
-      template: "<style>.popup { text-align:center;}</style>" + messege + "",
-      cssClass: "confirmPopup",
-      buttons: [
-        {
-          text: "OK",
-          type: "button-outline button-positive OutbuttonSize OutbuttonSizeFirst",
-          onTap: function (e) {
+    // ============= glob ========================
+    $rootScope.alert = function(messege) {
+        if (!isEmpty($scope.alertPopup)) {
+            $scope.alertPopup.close();
+        }
+        $scope.alertPopup = $ionicPopup.alert({
+            title: "",
+            template: "<style>.popup { text-align:center;}</style>" + messege + "",
+            cssClass: "confirmPopup",
+            buttons: [{
+                text: "OK",
+                type: "button-outline button-positive OutbuttonSize OutbuttonSizeFirst",
+                onTap: function(e) {
+                    return true;
+                },
+            }, ],
+        });
+    };
+    $rootScope.checkLoginUserReq = function() {
+        if (isEmpty($rootScope.loginUserInfo.incometypeid)) {
+            return false;
+        } else if (isEmpty($rootScope.loginUserInfo.monthlyincome)) {
+            return false;
+        } else if (isEmpty($rootScope.loginUserInfo.totalincomehousehold)) {
+            return false;
+        } else if (isEmpty($rootScope.loginUserInfo.monthlypayment)) {
+            return false;
+        } else {
             return true;
-          },
-        },
-      ],
-    });
-  };
-  $rootScope.checkLoginUserReq = function () {
-    if (isEmpty($rootScope.loginUserInfo.incometypeid)) {
-      return false;
-    } else if (isEmpty($rootScope.loginUserInfo.monthlyincome)) {
-      return false;
-    } else if (isEmpty($rootScope.loginUserInfo.totalincomehousehold)) {
-      return false;
-    } else if (isEmpty($rootScope.loginUserInfo.monthlypayment)) {
-      return false;
-    } else {
-      return true;
+        }
+    };
+    $rootScope.checkLoginUserDatas = function(correntPath, next) {
+        if (!isEmpty($rootScope.loginUserInfo)) {
+            if ($rootScope.checkLoginUserReq()) {
+                return { now: next, nextpath: next };
+            } else {
+                return { now: "profile", nextpath: next };
+            }
+        } else {
+            $rootScope.hideFooter = true;
+            return { now: "sign", nextpath: next };
+        }
+    };
+    $rootScope.ShowLoader = function() {
+        $ionicLoading.show({
+            showBackdrop: true,
+            showDelay: 0,
+            template: '<div class="loadingio-spinner-spinner-v5m2iut1fj" > <div class="ldio-agte97219x" ><div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div ></div > </div >',
+        });
+    };
+    //========= first run ==========================
+    var basket = localStorage.getItem("basketData");
+    // console.log("basket", basket);
+    // console.log("localStorage", localStorage);
+    if (!isEmpty(basket)) $rootScope.basketData = JSON.parse(basket);
+    else $rootScope.basketData = [];
+
+    $scope.getAllBankList = function() {
+        serverDeferred.request("PL_MDVIEW_004", { systemmetagroupid: "1603958798356" }).then(function(response) {
+            $rootScope.allBankList = response;
+            // console.log("$rootScope.allBankList", $rootScope.allBankList);
+        });
+    };
+
+    //$scope.getAllBankList();
+
+    var bannerNotShow = localStorage.getItem("bannerNotShow");
+    if (bannerNotShow == "true") {
+        $scope.showlogin();
     }
-  };
-  $rootScope.checkLoginUserDatas = function (correntPath, next) {
-    if (!isEmpty($rootScope.loginUserInfo)) {
-      if ($rootScope.checkLoginUserReq()) {
-        return { now: next, nextpath: next };
-      } else {
-        return { now: "profile", nextpath: next };
-      }
-    } else {
-      $rootScope.hideFooter = true;
-      return { now: "sign", nextpath: next };
-    }
-  };
-  $rootScope.ShowLoader = function () {
-    $ionicLoading.show({
-      showBackdrop: true,
-      showDelay: 0,
-      template: '<div class="loadingio-spinner-spinner-v5m2iut1fj" > <div class="ldio-agte97219x" ><div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div > <div > </div ></div > </div >',
-    });
-  };
-  //========= first run ==========================
-  var basket = localStorage.getItem("basketData");
-  // console.log("basket", basket);
-  // console.log("localStorage", localStorage);
-  if (!isEmpty(basket)) $rootScope.basketData = JSON.parse(basket);
-  else $rootScope.basketData = [];
 
-  $scope.getAllBankList = function () {
-    serverDeferred.request("PL_MDVIEW_004", { systemmetagroupid: "1603958798356" }).then(function (response) {
-      $rootScope.allBankList = response;
-      // console.log("$rootScope.allBankList", $rootScope.allBankList);
-    });
-  };
-
-  $scope.getAllBankList();
-
-  var bannerNotShow = localStorage.getItem("bannerNotShow");
-  if (bannerNotShow == "true") {
-    $scope.showlogin();
-  }
-
-  $scope.callComingSoon = function () {
-    $rootScope.alert("Тун удахгүй");
-  };
+    $scope.callComingSoon = function() {
+        $rootScope.alert("Тун удахгүй");
+    };
 });
