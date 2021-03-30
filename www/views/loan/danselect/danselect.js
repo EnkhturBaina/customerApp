8878b5f380f6260f38c982b0d6d10f68784a8233
Ganbayaraas <ganbayaraas@yahoo.com>
2021-03-30 10:28:33 +0800

    asd

--- www/views/loan/danselect/danselect.js
+++ www/views/loan/danselect/danselect.js
@@ -1,1 +1,75 @@
-app.controller("danselectCtrl", function ($scope, $state, $stateParams, $rootScope, serverDeferred, $ionicPlatform, $ionicModal, $timeout) {});
+app.controller("danselectCtrl", function($scope, $state, $stateParams, $rootScope, serverDeferred, $ionicPlatform, $ionicModal, $timeout) {
+
+    $scope.gotoDanLogin = function() {
+        serverDeferred.carCalculation({ type: "auth_car_collateral", redirect_uri: "customerapp" }, "https://services.digitalcredit.mn/api/v1/c").then(function(response) {
+            $rootScope.stringHtmlsLink = response.result.data;
+            var authWindow = cordova.InAppBrowser.open($rootScope.stringHtmlsLink.url, "_blank", "location=no,toolbar=no");
+            $(authWindow).on("loadstart", function(e) {
+                var url = e.originalEvent.url;
+                var code = url.indexOf("https://services.digitalcred");
+                var error = /\?error=(.+)$/.exec(url);
+                if (code == 0 || error) {
+                    authWindow.close();
+                }
+
+                if (code == 0) {
+                    serverDeferred.carCalculation({ state: $rootScope.stringHtmlsLink.state }, "https://services.digitalcredit.mn/api/sso/check").then(function(response) {
+                        if (!isEmpty(response.result.data)) {
+                            var userInfo = response.result.data.info;
+                            if (!isEmpty(response.result.data.vehicle)) {
+                                $rootScope.userVehicleData = JSON.parse(response.result.data.vehicle);
+                                $rootScope.newCarReq = $rootScope.userVehicleData.list[0];
+                                if (!isEmpty(userInfo)) {
+                                    $scope.registerFunction(JSON.parse(userInfo));
+                                }
+                            }
+                        }
+                    });
+                } else if (error) {
+                    console.log(error);
+                }
+            });
+        });
+    };
+    $scope.registerFunction = function(value) {
+
+        var json = {
+            customerCode: value.regnum.toUpperCase(),
+            siRegNumber: value.regnum.toUpperCase(),
+            isActive: "1",
+        };
+        json.dcApp_crmUser_dan = {
+            userName: value.regnum.toUpperCase(),
+            userId: "1",
+            isActive: "1",
+        };
+        json.dcApp_crmUser_dan.dcApp_dcCustomer_dan = {
+            familyName: value.surname,
+            firstName: value.firstname,
+            lastName: value.lastname,
+            birthDate: value.birthDateAsText.substring(0, 10),
+            uniqueIdentifier: value.regnum.toUpperCase(),
+            profilePictureClob: value.image,
+            isActive: "1",
+            customerTypeId: "1",
+        };
+
+        serverDeferred.requestFull("dcApp_getCustomerRegistered_004", { uniqueIdentifier: value.regnum.toUpperCase() }).then(function(checkedValue) {
+            console.log("checkedValue", checkedValue);
+            if (!isEmpty(checkedValue[1]) && !isEmpty(checkedValue[1].customerid)) {
+                json.id = checkedValue[1].customerid;
+                json.dcApp_crmUser_dan.id = checkedValue[1].custuserid;
+                json.dcApp_crmUser_dan.dcApp_dcCustomer_dan.id = checkedValue[1].dccustomerid;
+            }
+
+            serverDeferred.requestFull("dcApp_crmCustomer_dan_001", json).then(function(response) {
+                if (!isEmpty(response) && !isEmpty(response[0])) {
+                    $rootScope.loginUserInfo = response[1];
+                    $rootScope.loginUserInfo.id = response[1].dcapp_crmuser_dan.id;
+                    localStorage.setItem("loginUserInfo", JSON.stringify($rootScope.loginUserInfo));
+                    $state.go('car_coll')
+                }
+            });
+        });
+    };
+});
--- www/views/other_goods/other_goods.js
+++ www/views/other_goods/other_goods.js
@@ -1,82 +1,82 @@
-angular.module("addOtherGoods.Ctrl", []).controller("addOtherGoodsCtrl", function ($rootScope, serverDeferred, $scope, $state, $ionicPopup) {
-  $rootScope.otherGoods = [];
-  $rootScope.newCarReq = {};
-  $rootScope.otherGoodsData = [];
+angular.module("addOtherGoods.Ctrl", []).controller("addOtherGoodsCtrl", function($rootScope, serverDeferred, $scope, $state, $ionicPopup) {
+    $rootScope.otherGoods = [];
+    $rootScope.newCarReq = {};
+    $rootScope.otherGoodsData = [];
 
-  $rootScope.suppliers = [];
-  $rootScope.getLocalGoodsData = function () {
-    $rootScope.otherGoodsData = JSON.parse(localStorage.getItem("otherGoods"));
-    // console.log("otherGoodsData", otherGoodsData);
-    console.log("$rootScope.otherGoodsData", $rootScope.otherGoodsData);
-    serverDeferred.request("PL_MDVIEW_004", { systemmetagroupid: "1614232214127503" }).then(function (response) {
-      $rootScope.suppliers = response;
-      // console.log("allSupplierList respionse", response);
-    });
-  };
-  $rootScope.getLocalGoodsData();
-  //Нийт үнэ
-  $rootScope.sumPrice = 0;
-  $rootScope.calcTotalPrice = function () {
+    $rootScope.suppliers = [];
+    $rootScope.getLocalGoodsData = function() {
+        $rootScope.otherGoodsData = JSON.parse(localStorage.getItem("otherGoods"));
+        // console.log("otherGoodsData", otherGoodsData);
+        console.log("$rootScope.otherGoodsData", $rootScope.otherGoodsData);
+        serverDeferred.request("PL_MDVIEW_004", { systemmetagroupid: "1614232214127503" }).then(function(response) {
+            $rootScope.suppliers = response;
+            // console.log("allSupplierList respionse", response);
+        });
+    };
+    $rootScope.getLocalGoodsData();
+    //Нийт үнэ
     $rootScope.sumPrice = 0;
-    var local = localStorage.getItem("otherGoods");
-    if (!isEmpty(local) && local != "undefined") {
-      $rootScope.otherGoods = JSON.parse(localStorage.getItem("otherGoods"));
-      for (var i = 0; i < $rootScope.otherGoods.length; i++) {
-        $rootScope.sumPrice += parseInt($rootScope.otherGoods[i].unitPrice);
-      }
-    } else if ($rootScope.otherGoods.length == 0) {
-      $rootScope.sumPrice = 0;
-    }
-  };
-  $rootScope.calcTotalPrice();
-
-  $scope.nexClivk = function () {
-    if (!isEmpty($rootScope.otherGoodsData)) {
-      var next = $rootScope.checkLoginUserDatas("otherGoods", "autoleasing-2");
-      localStorage.setItem("requestType", "consumer");
-      $state.go(next.now, { path: next.nextpath });
-      $rootScope.getLoanAmountFunc();
-      // $state.go("autoleasing-2");
-    } else {
-      $rootScope.alert("Та зээлээр авах бараагаа бүртгэнэ үү", "warning");
-    }
-  };
-
-  $scope.otherGoodsDelete = function (id) {
-    $ionicPopup.show({
-      template: "<b>Бараа устгах уу ?</b>",
-      cssClass: "confirmPopup",
-      buttons: [
-        {
-          text: "Үгүй",
-          type: "button-decline",
-        },
-        {
-          text: "Устгах",
-          type: "button-confirm",
-          onTap: function () {
-            for (var i = 0; i < $rootScope.otherGoodsData.length; i++) {
-              if ($rootScope.otherGoodsData[i].itemid === id) {
-                $rootScope.otherGoodsData.splice(i, 1);
-
-                localStorage.removeItem("otherGoods");
-                localStorage.setItem("otherGoods", JSON.stringify($rootScope.otherGoodsData));
-
-                $rootScope.calcTotalPrice();
-                $rootScope.getLocalGoodsData();
-              }
+    $rootScope.calcTotalPrice = function() {
+        $rootScope.sumPrice = 0;
+        var local = localStorage.getItem("otherGoods");
+        if (!isEmpty(local) && local != "undefined") {
+            $rootScope.otherGoods = JSON.parse(localStorage.getItem("otherGoods"));
+            for (var i = 0; i < $rootScope.otherGoods.length; i++) {
+                $rootScope.sumPrice += parseInt($rootScope.otherGoods[i].unitPrice);
             }
-          },
-        },
-      ],
-    });
-  };
+        } else if ($rootScope.otherGoods.length == 0) {
+            $rootScope.sumPrice = 0;
+        }
+    };
+    $rootScope.calcTotalPrice();
 
-  $scope.selectItemConsumer = function (item, aa) {
-    console.log(item);
-    console.log("aa", aa);
-  };
-  $scope.backFromOtherGoods = function () {
-    $state.go("home");
-  };
+    $scope.nexClivk = function() {
+        if (!isEmpty($rootScope.otherGoodsData)) {
+            var next = $rootScope.checkLoginUserDatas("otherGoods", "autoleasing-2");
+            localStorage.setItem("requestType", "consumer");
+            // $rootScope.getLoanAmountFunc();
+            $state.go(next.now, { path: next.nextpath }, { reload: true });
+
+            // $state.go("autoleasing-2");
+        } else {
+            $rootScope.alert("Та зээлээр авах бараагаа бүртгэнэ үү", "warning");
+        }
+    };
+
+    $scope.otherGoodsDelete = function(id) {
+        $ionicPopup.show({
+            template: "<b>Бараа устгах уу ?</b>",
+            cssClass: "confirmPopup",
+            buttons: [{
+                    text: "Үгүй",
+                    type: "button-decline",
+                },
+                {
+                    text: "Устгах",
+                    type: "button-confirm",
+                    onTap: function() {
+                        for (var i = 0; i < $rootScope.otherGoodsData.length; i++) {
+                            if ($rootScope.otherGoodsData[i].itemid === id) {
+                                $rootScope.otherGoodsData.splice(i, 1);
+
+                                localStorage.removeItem("otherGoods");
+                                localStorage.setItem("otherGoods", JSON.stringify($rootScope.otherGoodsData));
+
+                                $rootScope.calcTotalPrice();
+                                $rootScope.getLocalGoodsData();
+                            }
+                        }
+                    },
+                },
+            ],
+        });
+    };
+
+    $scope.selectItemConsumer = function(item, aa) {
+        console.log(item);
+        console.log("aa", aa);
+    };
+    $scope.backFromOtherGoods = function() {
+        $state.go("home");
+    };
 });
